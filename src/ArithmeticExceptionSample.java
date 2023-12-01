012b0b5f5d6bb49c59cdd3edd14555b150335b62
teramoto-m <applepanda66@gmail.com>
2023-12-02 03:47:40 +0900

    Javaプログラム入門学習終了

---
+++ src/ArithmeticExceptionSample.java
@@ -0,0 +1,15 @@
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        //double型10.0をdouble型0.0で除算
+        double a=10.0/0.0; //この場合は例外が発生しない 
+        
+        // この下の出力は実行される
+        System.out.println(a);
+        
+        
+
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("こんにちは！");
+        System.out.println("元気ですか");
+    }
+}
